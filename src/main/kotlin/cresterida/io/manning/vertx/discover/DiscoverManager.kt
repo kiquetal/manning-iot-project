9e623a2f5528341dbfdb741720ddd577d775bb88
Enrique M. Talavera <kiquetal@gmail.com>
2022-02-27 01:19:46 -0300

    Reading about servicediscovery

--- src/main/kotlin/cresterida/io/manning/vertx/discover/DiscoverManager.kt
+++ src/main/kotlin/cresterida/io/manning/vertx/discover/DiscoverManager.kt
@@ -1,7 +1,15 @@
 package cresterida.io.manning.vertx.discover
 
-import io.vertx.ext.web.Router
 
 class DiscoverManager{
+
+
+  companion object {
+
+
+    fun getService(vertObject: vertx):ServiceDiscovery {
+
+    }
+  }
 
 }
--- src/main/kotlin/cresterida/io/manning/vertx/http/Registration.kt
+++ src/main/kotlin/cresterida/io/manning/vertx/http/Registration.kt
@@ -1,4 +1,16 @@
 package cresterida.io.manning.vertx.http
 
-class Registration {
+open class Registration {
+
+
+  companion object {
+    @JvmStatic
+    fun main(args:Array<String>)
+    {
+      println("let test it")
+
+    }
+  }
+
+
 }
