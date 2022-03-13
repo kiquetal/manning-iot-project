f24df11984a230dab7c988537322865b2746ffcf
Enrique M. Talavera <kiquetal@gmail.com>
2022-03-13 11:15:31 -0300

    Adding json object

--- src/main/kotlin/cresterida/io/manning/vertx/http/Registration.kt
+++ src/main/kotlin/cresterida/io/manning/vertx/http/Registration.kt
@@ -5,9 +5,12 @@ import io.vertx.core.Handler
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpMethod
+import io.vertx.core.json.Json
 import io.vertx.ext.web.Route
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.jsonObjectOf
 import java.util.concurrent.CompletableFuture
 
  object Registration {
@@ -23,7 +26,10 @@ import java.util.concurrent.CompletableFuture
 
   fun Route.proccessServer() {
     handler { ctx: RoutingContext ->
-      ctx.response().end(ctx.body)
+      println("recibido")
+      val obj = json { jsonObjectOf("name" to "kiquetal-soy yo") }
+      ctx.response().putHeader("Content-type","application/json")
+      ctx.response().end(obj.encode())
     }
   }
 
